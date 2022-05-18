c14f1bdb3a7fae2424267d664939e84a597ce077
GabrielGhi <75260000+GabrielGhi@users.noreply.github.com>
2022-05-18 09:15:50 -0300

    Prova do crl

--- App.js
+++ App.js
@@ -1,112 +1,18 @@
-import * as React from 'react';
-import { Button, View, Text, ImageBackground } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import React from 'react';
 
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        onPress={() => navigation.navigate('Notifications')}
-        title="Go to notifications"
-      />
-      <Text>{'\n'}</Text>
-      
-      <Button
-        title="Go to Login"
-        onPress={() => navigation.navigate('Login')}
-      />
-    </View>
-  );
-}
-
-function NotificationsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Notifications Screen</Text>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
-      <Button onPress={() => navigation.navigate('Settings')} title="Go to Settings" />
-    </View>
-  );
-}
-
-const Drawer = createDrawerNavigator();
-
-function MainDrawer() {
-  return (
-
-    <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
-        <Drawer.Screen name="Settings" component={Settings} />
-        <Drawer.Screen name="Cadastro" component={Cadastro} />
-    </Drawer.Navigator>
-  );
-}
-
-
-function Cadastro() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Profile Screen</Text>
-
-      <ImageBackground  
-    source={require('./assets/escola1.jpg')} 
-    style={{  width: 1800, height: 850, justifyContent: 'center', alignItems: 'center',}}>
-      <Button title="Left button" onPress={() => Alert.alert('Left button pressed')} />
-    </ImageBackground>
-    </View>
-  );
-}
-
-function Settings() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Settings Screen</Text>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
-    </View>
-  );
-}
-
-function Login({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Login Screen</Text>
-      <Button
-        title="Go to MainDrawer"
-        onPress={() => navigation.navigate('MainDrawer')}
-      />
-      <Button
-        onPress={() => navigation.navigate('Cadastro')}
-        title="Go to Screen Of Sign Up"
-      />
-
-      
-    </View>
-  );
-}
-
-const Stack = createNativeStackNavigator();
+import AuthStack from './src/navigation/AuthStack';
+import AppStack from './src/navigation/AppStack';
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          name="MainDrawer"
-          component={MainDrawer}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Cadastro" component={Cadastro} />
-        <Stack.Screen name="Settings" component={Settings} />
-      </Stack.Navigator>
+      {/* <AppStack /> */}
+      <AuthStack />
     </NavigationContainer>
   );
 }
 
 export default App;
-
